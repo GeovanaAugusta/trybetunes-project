09657c7e042800e2774589a9cd894275be026758
Geovana Augusta <geovanaag@hotmail.com>
2022-04-18 23:14:25 -0300

    Requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -36,6 +37,9 @@ class Header extends React.Component {
               </h1>
             </div>
           )}
+        <Link data-testid="link-to-search" to="/search">Search</Link>
+        <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+        <Link data-testid="link-to-profile" to="/profile">Profile</Link>
       </header>
 
     );
